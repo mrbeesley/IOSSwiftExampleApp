b5b963868418cb91b616e9ff14dad4e5062a557c
Michael Beesley <mbeesley@smg.com>
2017-08-02 07:57:31 -0500

    Missed changes for initial check in

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
     
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -28,12 +28,29 @@ class RatingControl: UIStackView {
     //MARK: Properties
     private var ratingButtons = [UIButton]()
     var rating = 0
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet{
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet{
+            setupButtons()
+        }
+    }
     
     //MARK: Private Methods
     private func setupButtons(){
         
+        // remove existing buttons so they don't get double added
+        for button in ratingButtons{
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
         // create x number of buttons
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
 
             // create the button
             let button = UIButton()
@@ -41,8 +58,8 @@ class RatingControl: UIStackView {
             
             // add constraints to the button
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             // setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
